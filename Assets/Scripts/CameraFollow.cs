291b5ebd9041359e01712cb756f85f7cf34bb37d
Charles Zhai <charlie.zhaibca@gmail.com>
2018-03-17 02:44:36 -0600

    SpaceMap

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform spherePos;
+
+	private float zDistance = 10f;
+	private float yDistance = 4f;
+
+	void Awake() {
+		spherePos = GameObject.Find("Sphere").transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = transform.position;
+		temp.x = spherePos.position.x;
+		temp.y = spherePos.position.y + yDistance;
+		temp.z = spherePos.position.z - zDistance;
+		transform.position = temp;
+	}
+}
---
+++ Assets/Scripts/Level Generator Scripts/LevelGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	[SerializeField]
+	private int LevelLength;
+
+	[SerializeField]
+	private int startPlatformLength = 5, endPlatformLength = 5;
+
+}
