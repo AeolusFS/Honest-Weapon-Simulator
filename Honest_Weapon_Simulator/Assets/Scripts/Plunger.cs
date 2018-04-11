e5216aa3a9cea2b053e9b48a23a4bb0be14e6e25
Charles Zhai <charlie.zhaibca@gmail.com>
2018-04-11 14:55:03 -0600

    plugger and shield_sword

---
+++ Honest_Weapon_Simulator/Assets/Scripts/Plunger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plunger : InventoryItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "Plunger";
+        }
+    }
+
+}
